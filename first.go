aa02e53f8b28e1285e95fa16d33012e6e81c21ce
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-06-01 00:28:09 +0700

    Output format

--- first.go
+++ first.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var name = "Go conference" // Neu nhu khong su dung bien nay thi se thong bao loi
+	const conferenceTickets = 50
+	fmt.Println("Hello ", name, " World")
+	fmt.Println("Get Your tickets % here to attend", name)
+	fmt.Printf("Get Your tickets %v here to attend\n", name)
+
+	fmt.Print(name)
 }
